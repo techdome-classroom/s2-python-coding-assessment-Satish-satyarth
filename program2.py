cbc4261f6e77e46b5f4f3ff0996ba4d54f220f85
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 14:28:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        # Dictionary for Roman numeral values
         roman_dict = {
             'I': 1,
             'V': 5,
@@ -17,37 +17,13 @@ class Solution(object):
 
         total = 0
 
-        
+        # Loop through the Roman numeral string
         for i in range(len(s)):
-            
+            # If the current Roman numeral is smaller than the next one, subtract it
             if i + 1 < len(s) and roman_dict[s[i]] < roman_dict[s[i + 1]]:
                 total -= roman_dict[s[i]]
             else:
-                
+                # Otherwise, add its value to the total
                 total += roman_dict[s[i]]
 
         return total
-
-
-def isValid(s: str) -> bool:
-    
-    bracket_map = {')': '(', '}': '{', ']': '['}
-
-    
-    stack = []
-
-    
-    for char in s:
-        if char in bracket_map:
-            
-            top_element = stack.pop() if stack else '#'
-
-            
-            if bracket_map[char] != top_element:
-                return False
-        else:
-            
-            stack.append(char)
-
-    
-    return not stack
