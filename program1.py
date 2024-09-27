199a269d42a7a07d7f7aa55fea9d426d50eebe11
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 14:16:56 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
- 
+        function romanToInt(s) {
+    // Dictionary for Roman numeral values
+    const romanDict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+
+    // Loop through the Roman numeral string
+    for (let i = 0; i < s.length; i++) {
+        // If the current Roman numeral is smaller than the next one, subtract it
+        if (i + 1 < s.length && romanDict[s[i]] < romanDict[s[i + 1]]) {
+            total -= romanDict[s[i]];
+        } else {
+            // Otherwise, add its value to the total
+            total += romanDict[s[i]];
+        }
+    }
+
+    return total;
+}
 
         pass
 
